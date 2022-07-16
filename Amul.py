f87b4e12c28f1654551f7e243a5eee1620843938
amulsharma2i3 <108885415+amulsharma2i3@users.noreply.github.com>
2022-07-16 18:13:48 +0545

    Amul.py
    $ pkg update && pkg upgrade
    $ pkg install python
    $ pip install mechanize lolcat
    $ pip install requests bs4
    $ pip install futures
    $ pkg install git
    $ git clone git@github.com:amulsharma2i3/AMUL-XD-.git

---
+++ Amul.py
@@ -0,0 +1,131 @@
+<img src="https://github.com/Amul-vau/Amul-vau/blob/main/IMAGE/amulvau.gif" width="120" height="120" align="left">
+
+<center>
+
+  
+
+  
+
+ 
+
+   ##  MY SOCIAL MEDIA : <br>
+
+<a href="https://Instagram.com/azimmahmud143" target="_blank"><img src="https://github.com/Azim-vau/Azim-vau/blob/main/IMAGE/instagram.png" alt="alt text" width="25" height="25"></a> 
+
+<a href="https://t.me/mrerror69"><img src="https://github.com/Azim-vau/Azim-vau/blob/main/IMAGE/telegram.png" alt="alt text" width="25" height="25"></a>
+
+<a href="https://www.facebook.com/azimmahmudofficial" target="_blank"><img src="https://github.com/Azim-vau/Azim-vau/blob/main/IMAGE/facebook.png" alt="alt text" width="25" height="25"></a> <a href="https://youtube.com/MrError69"><img src="https://github.com/Azim-vau/Azim-vau/blob/main/IMAGE/youtube.png" alt="alt text" width="25" height="25"></a> 
+
+&nbsp;&nbsp;     &nbsp;&nbsp;    &nbsp;&nbsp;   &nbsp;&nbsp;   &nbsp;&nbsp;
+
+  
+
+___Programming And Memes___
+
+CONTACT WITH <a href="https://github.com/Azim-vau"><b>MR. ERROR </a> </br><br>
+
+<img src="https://github.com/Azim-vau/Azim-vau/blob/main/IMAGE/contact.png" alt="alt text" width="25" height="25"> <br>
+
+<p>amuldon@gmail.com</p>  <br> <br> 
+
+<a href="https://github.com/Azim-Vau/followers">
+
+<img title="Followers" src="https://img.shields.io/github/followers/Azim-vau?label=Followers&color=blue&style=flat-square"></a>
+
+<br>
+
+  <a href="https://github.com/Azim-Vau/termux-style/stargazers/">
+
+  <a href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/Azim-vau/crack-pro.svg"/>
+
+  </a>
+
+<br>
+
+  <a href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Language" src="https://img.shields.io/github/languages/count/Azim-vau/crack-pro.svg"/>
+
+  </a>
+
+  <a href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Starts" src="https://img.shields.io/github/stars/Azim-vau/crack-pro.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Repo Size" src="https://img.shields.io/github/repo-size/Azim-vau/crack-pro.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Top Language" src="https://img.shields.io/github/languages/top/Azim-vau/crack-pro.svg"/> <a                                                                                                        href="https://github.com/Azim-vau/crack-pro">
+
+    <img alt="Forks" src="https://img.shields.io/github/forks/Azim-vau/crack-pro.svg"/>
+
+  </a>
+
+</div>
+
+</br>
+
+<p align="center">
+
+      FACEBOOK ACCOUNT CLONER
+
+</p>
+
+![20200808_160757](https://github.com/Amul-vau/MODULES/blob/main/Screenshot_20211024-173340_Termux.jpg)
+
+#### INSTALL TOOL ON TERMUX
+
+$ apt update && apt upgrade
+
+$ apt install python
+
+$ pip install mechanize lolcat
+
+$ pip install requests bs4
+
+$ pip install futures
+
+$ apt install git
+
+$ git clone https://github.com/Amul-vau/crack-pro.git
+
+#### RUN SCRIPT
+
+$ cd crack-pro
+
+$ python cr3k.py
+
+#### :closed_lock_with_key: KEY PASSWORD ⁉️
+
+#### JOIN TELEGRAM CHANNEL <br>
+
+[![](https://img.shields.io/badge/Telegram-black?logo=Telegram&logoColor=blue&labelColor=black)](https://t.me/mrerror69)
+
+<br>
+
+<b>🎯 GET ACCESS TOKEN APK <b/>
+
+<a href="https://play.google.com/store/apps/details?id=com.proit.thaison.getaccesstokenfacebook"> DOWNLOAD <a/> <br>
+
+#### MY SOCIAL MEDIA
+
+[![](https://img.shields.io/badge/Github-black?logo=Github&logoColor=red&labelColor=black)](https://github.com/Amul-Vau) <br>
+
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=red&labelColor=black)](https://www.facebook.com/amuldon) <br>
+
+[![](https://img.shields.io/badge/Instagram-black?logo=Instagram&logoColor=red&labelColor=black)](https://www.instagram.com/amuldon) <br>
+
+<h2> THANKS FOR VISIT ✘✘ <h2\>
